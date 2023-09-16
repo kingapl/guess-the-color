282d4cecc3abf0b59048fa72fed6bdeecf3d7ed4
kingapl <kingapluciennik@wp.pl>
2023-09-16 19:57:33 +0200

    Add app window

---
+++ app.py
@@ -0,0 +1,23 @@
+import pygame
+
+
+class App:
+    pygame.init()
+    screen = pygame.display.set_mode((1280, 720))
+
+    running = True
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        screen.fill((197, 202, 207))
+
+        pygame.display.flip()
+
+    pygame.quit()
+
+
+if __name__ == '__main__':
+    app = App()

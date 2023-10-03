11a545a556d7f5a62bb4fc65e910254a0c8a35fb
kingapl <kingapluciennik@wp.pl>
2023-10-03 15:08:12 +0200

    Add a point for each correct choice and display the points on the screen

--- text.py
+++ text.py
@@ -2,11 +2,13 @@ import pygame
 
 
 class Text:
-    def __init__(self, screen, text, color, font_size):
+    def __init__(self, screen, text, color, letters_color_name, font_size):
         self.screen = screen
         self.font = pygame.font.Font(pygame.font.get_default_font(), font_size)
         self.surface = self.font.render(text, True, color)
         self.width = self.surface.get_rect().width
+        self.text = text
+        self.letters_color_name = letters_color_name
 
     def display(self, x, y):
         self.screen.blit(self.surface, (x, y))
